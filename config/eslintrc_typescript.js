a6ea1532d74966783caa0f225c5f9f4d050b4b16
Tetsuharu OHZEKI <tetsuharu.ohzeki@gmail.com>
2019-03-05 17:39:18 +0900

    Add TODO items to decide to switch typescript-eslint rules
    By https://github.com/typescript-eslint/typescript-eslint/tree/00eae48cd64028238720e239f9ee04807c4311c4/packages/eslint-plugin

--- config/eslintrc_typescript.js
+++ config/eslintrc_typescript.js
@@ -10,6 +10,20 @@ module.exports = {
     ],
 
     'rules': {
+        // TODO: @typescript-eslint/adjacent-overload-signatures
+        // TODO: @typescript-eslint/array-type
+        // TODO: @typescript-eslint/ban-types
+        // TODO: @typescript-eslint/ban-ts-ignore
+        // TODO: @typescript-eslint/camelcase
+        // TODO: @typescript-eslint/class-name-casing
+        // TODO: @typescript-eslint/explicit-function-return-type
+        // TODO: @typescript-eslint/explicit-member-accessibility
+        // TODO: @typescript-eslint/generic-type-naming
+        // TODO: @typescript-eslint/indent
+        // TODO: @typescript-eslint/interface-name-prefix
+        // TODO: @typescript-eslint/member-delimiter-style
+        // TODO: @typescript-eslint/member-naming
+
         // I don't think it's not efffective to sort the order by public/private/protected.
         '@typescript-eslint/member-ordering': ['warn', {
             // * I'd like to aggregate instance fields
@@ -24,11 +38,30 @@ module.exports = {
             ],
         }],
 
+        // TODO: @typescript-eslint/no-angle-bracket-type-assertion
+        // TODO: @typescript-eslint/no-array-constructor
+        // TODO: @typescript-eslint/no-empty-interface
+
         // Please opt-out this rule if you don't have any workarounds.
         '@typescript-eslint/no-explicit-any': 'warn',
+
+        // TODO: @typescript-eslint/no-extraneous-class
+        // TODO: @typescript-eslint/no-for-in-array
+        // TODO: @typescript-eslint/no-inferrable-types
+        // TODO: @typescript-eslint/no-misused-new
+        // TODO: @typescript-eslint/no-namespace
+
         // Please opt-out this rule if you don't have any workarounds.
         '@typescript-eslint/no-non-null-assertion': 'warn',
 
+        // TODO: @typescript-eslint/no-object-literal-type-assertion
+        // TODO: @typescript-eslint/no-parameter-properties
+        // TODO: @typescript-eslint/no-require-imports
+        // TODO: @typescript-eslint/no-this-alias
+        // TODO: @typescript-eslint/no-triple-slash-reference
+        // TODO: @typescript-eslint/no-type-alias
+        // TODO: @typescript-eslint/no-unnecessary-qualifier
+        // TODO: @typescript-eslint/no-unnecessary-type-assertion
 
         // the default `no-unused-vars` is not support type annotations.
         'no-unused-vars': 'off',
@@ -41,7 +74,17 @@ module.exports = {
             'caughtErrorsIgnorePattern': '^_', // Allow `catch (_e) {...}`
         }],
 
+        // TODO: @typescript-eslint/no-use-before-define
+        // TODO: @typescript-eslint/no-useless-constructor
+        // TODO: @typescript-eslint/no-var-requires
+        // TODO: @typescript-eslint/prefer-function-type
+        // TODO: @typescript-eslint/prefer-interface
+
         // This bans legacy syntax.
         '@typescript-eslint/prefer-namespace-keyword': 'error',
+
+        // TODO: @typescript-eslint/promise-function-async
+        // TODO: @typescript-eslint/restrict-plus-operands
+        // TODO: @typescript-eslint/type-annotation-spacing
     }
 };
