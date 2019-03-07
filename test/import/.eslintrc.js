5aca930903b46b538acbc2dfb86f69d238a38901
Tetsuharu OHZEKI <tetsuharu.ohzeki@gmail.com>
2019-03-08 01:04:02 +0900

    Keep simple the top level .eslintrc.js
    For testing, we should use particular configs.

---
+++ test/import/.eslintrc.js
@@ -0,0 +1,18 @@
+// XXX: To uniform the style of an object literals, we enable `quote-props`
+/*eslint quote-props: [2, "always"] no-magic-numbers: 0 */
+
+// eslint-disable-next-line strict
+'use strict';
+
+/**
+ *  Test ourself :)
+ */
+module.exports = {
+    'extends': [
+        '../../config/eslintrc_import.js',
+    ],
+
+    'parserOptions': {
+        'sourceType': 'module',
+    },
+};
--- test/typescript/.eslintrc.js
+++ test/typescript/.eslintrc.js
@@ -1,7 +1,12 @@
 // XXX: To uniform the style of an object literals, we enable `quote-props`
 /*eslint quote-props: [2, "always"] no-magic-numbers: 0 */
+/* eslint-env node */
 
+// eslint-disable-next-line strict
 'use strict';
+
+// eslint-disable-next-line @typescript-eslint/no-require-imports
+const path = require('path');
 
 /**
  *  Test ourself :)
@@ -11,24 +16,14 @@ module.exports = {
     // Load all configs managed in this project
     // to check them by schemes by theirs.
     'extends': [
-        './config/eslintrc_core.js',
-        './config/eslintrc_node.js',
+        '../../config/eslintrc_typescript.js',
+        '../../config/eslintrc_typescript_react.js',
     ],
 
     // This option is only applied to this project
     // (This will not be applied for an user project).
     'parserOptions': {
-        'ecmaVersion': 2018,
-        'sourceType': 'script',
+        'sourceType': 'module',
+        'project': path.resolve(__dirname, '../../tsconfig.json'),
     },
-
-    'plugins': [
-        'markdown',
-    ],
-
-    'env': {
-        'commonjs': true,
-    },
-
-    'root': true,
 };
