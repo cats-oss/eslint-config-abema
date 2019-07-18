bfd4a82cc008674fbaaa371f147d5e870fb103b9
Tetsuharu OHZEKI <tetsuharu.ohzeki@gmail.com>
2019-07-18 21:23:46 +0900

    [typescript-eslint] Migrate to triple-slash-reference rule

--- config/eslintrc_typescript.js
+++ config/eslintrc_typescript.js
@@ -180,9 +180,6 @@ module.exports = {
             'allowedNames': ['self'],
         }],
 
-        // Basically, use ES Module import. // <reference path="" /> is just special case.
-        '@typescript-eslint/no-triple-slash-reference': 'error',
-
         // Disabling this does not make sense completely.
         '@typescript-eslint/no-type-alias': 'off',
 
@@ -293,6 +290,13 @@ module.exports = {
 
         // This detects a common mistake which uses `+` for diffrent types.
         '@typescript-eslint/restrict-plus-operands': 'warn',
+
+        // Basically, use ES Module import. // <reference path="" /> is just special case.
+        '@typescript-eslint/triple-slash-reference': ['error', {
+            'path': 'never',
+            'types': 'never',
+            'lib': 'never',
+        }],
 
         // TODO: @typescript-eslint/type-annotation-spacing
 
