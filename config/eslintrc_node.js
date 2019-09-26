a63426e01fe1cc68bc2be624260c5a7348847fc4
Tetsuharu OHZEKI <tetsuharu.ohzeki@gmail.com>
2019-09-26 15:56:25 +0900

    Disable node/no-callback-literal

--- config/eslintrc_node.js
+++ config/eslintrc_node.js
@@ -14,6 +14,7 @@ module.exports = {
     /* eslint sort-keys: 'error' */
     'rules': {
         'node/exports-style': [1, 'module.exports'],
+        'node/no-callback-literal': 'off',
         'node/no-deprecated-api': 2, // we'd like to detect the case of using deprecated apis.
         'node/no-exports-assign': 'error', // This simply checks human error.
         'node/no-extraneous-import': 2, // Specify more details in your project.
