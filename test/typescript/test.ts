2c42781cf68e3c395de0ccdd5557c58ffb79316a
Tetsuharu Ohzeki <tetsuharu.ohzeki@gmail.com>
2020-05-26 09:13:19 +0900

    Use PascalCase for TypeScript's enum member

--- test/typescript/test.ts
+++ test/typescript/test.ts
@@ -1,6 +1,15 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 // Only for testing to avoid the compile error by typescript compiler
 // if there it no input
 
 /* eslint-env node */
 
 console.log('test');
+
+enum BarEnum {
+  A = 1,
+}
+
+const enum ConstBarEnum {
+  A = '1',
+}
